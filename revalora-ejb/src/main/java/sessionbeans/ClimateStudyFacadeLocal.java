99d2983dca084daa46e616f2eab31f40fdc6242e
Mario Luis <mariolopezlandes@gmail.com>
2014-05-21 23:38:28 -0400

    Refactorización de Study>ClimateStudy

---
+++ revalora-ejb/src/main/java/sessionbeans/ClimateStudyFacadeLocal.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sessionbeans;
+
+import entities.ClimateStudy;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author pingeso
+ */
+@Local
+public interface ClimateStudyFacadeLocal {
+
+    void create(ClimateStudy climateStudy);
+
+    void edit(ClimateStudy climateStudy);
+
+    void remove(ClimateStudy climateStudy);
+
+    ClimateStudy find(Object id);
+
+    List<ClimateStudy> findAll();
+
+    List<ClimateStudy> findRange(int[] range);
+
+    int count();
+    
+}
