4729ac257ad90444f3b8cad6b0f6e3f9b359239c
Gustavo Salvo <gsalvo@vasal.cl>
2014-05-22 00:40:54 -0400

    creaión de send

--- revalora-web/src/main/java/managedbeans/mailers/MenuMail.java
+++ revalora-web/src/main/java/managedbeans/mailers/MenuMail.java
@@ -30,32 +30,6 @@ public class MenuMail {
      * Creates a new instance of MenuMail
      */
     public MenuMail() {
-        root = new TreeNode(root);
-        root.
-        TreeNode documents = new TreeNode("Documents", root);
-        TreeNode pictures = new TreeNode("Pictures", root);
-        TreeNode music = new TreeNode("Music", root);
-        TreeNode work = new TreeNode("Work", documents);
-        TreeNode primefaces = new TreeNode("PrimeFaces", documents);
-//Documents
-        TreeNode expenses = new TreeNode("document", "Expenses.doc", work);
-        TreeNode resume = new TreeNode("document", "Resume.doc", work);
-        TreeNode refdoc = new TreeNode("document", "RefDoc.pages", primefaces);
-//Pictures
-        TreeNode barca = new TreeNode("picture", "barcelona.jpg", pictures);
-        TreeNode primelogo = new TreeNode("picture", "logo.jpg", pictures);
-        TreeNode optimus = new TreeNode("picture", "optimus.png", pictures);
-//Music
-        TreeNode turkish = new TreeNode("Turkish", music);
-        TreeNode cemKaraca = new TreeNode("Cem Karaca", turkish);
-        TreeNode erkinKoray = new TreeNode("Erkin Koray", turkish);
-        TreeNode mogollar = new TreeNode("Mogollar", turkish);
-        TreeNode nemalacak = new TreeNode("mp3", "Nem Alacak Felek Benim", cemKaraca);
-        TreeNode resimdeki = new TreeNode("mp3", "Resimdeki Goz Yaslari", cemKaraca);
-        TreeNode copculer = new TreeNode("mp3", "Copculer", erkinKoray);
-        TreeNode oylebirgecer = new TreeNode("mp3", "Oyle Bir Gecer", erkinKoray);
-        TreeNode toprakana = new TreeNode("mp3", "Toprak Ana", mogollar);
-        TreeNode bisiyapmali = new TreeNode("mp3", "Bisi Yapmali", mogollar);
     }
 
 }
