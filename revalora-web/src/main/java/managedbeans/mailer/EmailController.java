f7e73698d1ceed30e015ba00c52e1b3cc6a76550
Gustavo Salvo <gsalvo@vasal.cl>
2014-07-01 22:50:25 -0400

    reenvio

--- revalora-web/src/main/java/managedbeans/mailer/EmailController.java
+++ revalora-web/src/main/java/managedbeans/mailer/EmailController.java
@@ -10,17 +10,26 @@ import entities.Contact;
 import entities.ContactGroup;
 import java.io.IOException;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
 import managedbeans.util.JsfUtil;
 import managedbeans.util.SessionUtil;
+import org.primefaces.component.autocomplete.AutoComplete;
 import otherclasses.Email;
 import sessionbeans.util.EmailSessionBeanLocal;
 
@@ -32,7 +41,10 @@ import sessionbeans.util.EmailSessionBeanLocal;
 @SessionScoped
 public class EmailController implements Serializable {
 
-    private Email selected;
+    private Email selected,draft;
+    private List<Contact> contacts = new ArrayList<>();
+    private List<ContactGroup> groups = new ArrayList<>();
+    private boolean disableDraft = false;
     
     @EJB
     private EmailSessionBeanLocal emailSessionBean;
@@ -44,6 +56,14 @@ public class EmailController implements Serializable {
     private EmailInbox emailInbox;
     
     public EmailController() {
+    }
+
+    public Email getDraft() {
+        return draft;
+    }
+
+    public void setDraft(Email draft) {
+        this.draft = draft;
     }
 
     public Email getSelected() {
@@ -60,7 +80,7 @@ public class EmailController implements Serializable {
         selected = new Email();
         return selected;
     }
-    
+
     public void createSendSurvey(List<Contact> contacts, List<ContactGroup> cGroups, String subject, String content) {
         selected = prepareCreate();
         selected.setReceiverContactGroups(cGroups);
@@ -90,9 +110,11 @@ public class EmailController implements Serializable {
     }
     
     private void send(String successMessage) throws MessagingException, IOException{
+        //Para que no se guarde en borradores
+        disableDraft = true;
+        
         List<Contact> recipientContacts = selected.getReceiverContacts();
         List<ContactGroup> recipientGroups = selected.getReceiverContactGroups();
-        
         if(recipientContacts!=null){
             for (int i = 0; i < recipientContacts.size(); i++) {
                 sendMessage(recipientContacts.get(i).getEmail(),
@@ -114,10 +136,14 @@ public class EmailController implements Serializable {
                 }
             }
         }
+        //Reinicializamos valores
+        contacts = new ArrayList<>();
+        groups = new ArrayList<>();
+        selected = new Email();
+        disableDraft = false;
     }
     
-    public void sendMessage(String recipient, String subject, String content, String successMessage) throws IOException{
-        
+    public void sendMessage(String recipient, String subject, String content, String successMessage) throws IOException{   
         try {
             emailSessionBean.sendMail(recipient,"","","", subject, content);
             JsfUtil.addSuccessMessage(successMessage);
@@ -146,5 +172,47 @@ public class EmailController implements Serializable {
             }            
         }
         return false;
+    }
+
+    public void handleSelectedContact(AjaxBehaviorEvent theEvent) {
+        AutoComplete autoComplete = (AutoComplete) theEvent.getComponent();
+        contacts = (List<Contact>) autoComplete.getValue();
+    }
+    
+    public void handleSelectedGroup(AjaxBehaviorEvent theEvent) {
+        AutoComplete autoComplete = (AutoComplete) theEvent.getComponent();
+        groups = (List<ContactGroup>) autoComplete.getValue();
     }    
+    
+    public void deactivateDraft(){
+        disableDraft = true;
+    }
+    
+    public void activateDraft(){
+        disableDraft = false;
+    }
+    
+    public void saveToDraft() {
+        try {
+            //Esperamos por si la ventana se cerró al apretar enviar.
+            //Si es así, disableDraft será true, y se guardará como borrador
+            TimeUnit.SECONDS.sleep(2);
+            System.out.println("desactivar draft: "+disableDraft);
+            if(!disableDraft){
+                if(!selected.getSubject().isEmpty() || !selected.getContent().isEmpty() || contacts.isEmpty() || !groups.isEmpty()){
+                    emailSessionBean.saveMailToDraft(selected.getSubject(), selected.getContent(), contacts, groups);
+                    //Reinicializamos valores
+                    contacts = new ArrayList<>();
+                    groups = new ArrayList<>();
+                    selected = new Email();
+                    JsfUtil.addSuccessMessage("Se ha guardado el mensaje como borrador");
+                }
+            }
+            contacts = new ArrayList<>();
+            groups = new ArrayList<>();
+            selected = new Email();
+        }catch(InterruptedException ex){
+            JsfUtil.addErrorMessage("Error al guardar como borrador, intentelo más tarde");
+        }
+    }
 }
--- revalora-web/src/main/java/managedbeans/mailer/resendInvitation.java
+++ revalora-web/src/main/java/managedbeans/mailer/resendInvitation.java
@@ -13,6 +13,7 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,6 +24,7 @@ import javax.mail.MessagingException;
 import managedbeans.ClimateStudyController;
 import managedbeans.ClimateStudyInvitationController;
 import managedbeans.util.Crypto;
+import managedbeans.util.JsfUtil;
 import sessionbeans.ClimateStudyInvitationFacadeLocal;
 import sessionbeans.ClimateStudyParticipationFacadeLocal;
 import sessionbeans.util.EmailSessionBeanLocal;
@@ -40,8 +42,9 @@ public class resendInvitation implements Serializable {
     @EJB
     private ClimateStudyInvitationFacadeLocal ejbFacade;
 
-    private List<ClimateStudyInvitation> listInvitation = new ArrayList<ClimateStudyInvitation>();
-    private List<ClimateStudyInvitation> listReal = new ArrayList<ClimateStudyInvitation>();
+    private List<ClimateStudyInvitation> listInvitation = new ArrayList<>();
+    private List<ClimateStudyInvitation> listInvReady = new ArrayList<>();
+    private List<ClimateStudyInvitation> listRealTemp = new ArrayList<>();
 
     private final String basePath = "http://localhost:8080/revalora-web/faces/survey/climate.xhtml";
 
@@ -50,43 +53,72 @@ public class resendInvitation implements Serializable {
             Crypto cypher = new Crypto();
             listInvitation = ejbFacade.findAll();
             System.out.println(listInvitation.size());
-
-            for (int j = 0; j<listInvitation.size(); j++) {
-                if (listInvitation.get(j).getState() != null && listInvitation.get(j).getContact() != null) {
-                    long idClimateAux = listInvitation.get(j).getStudy().getId();
-                    String mailAux = listInvitation.get(j).getContact().getEmail();
-                    boolean isReady = false;
-                    System.out.println("inicio primer bucle: "+idClimateAux + " " + mailAux + " " + listInvitation.get(j).getState());
-                    for (int i = 0; i<listInvitation.size(); i++) {
-                        System.out.println(listInvitation.get(i).getStudy().getId()+ " " + listInvitation.get(i).getContact().getEmail() + " " + listInvitation.get(i).getState());
-                        if(listInvitation.get(i).getStudy().getId()== idClimateAux && listInvitation.get(i).getContact().getEmail().equals(mailAux) && listInvitation.get(i).getState().equals("Listo")){
-                            
-                            System.out.println("Esta LISTO");
-                            isReady = true;
-                            break;
-                        }                        
+            
+            //Obtenemos todas las encuestas que ya están listas
+            for (int i = 0; i<listInvitation.size(); i++) {
+                if (listInvitation.get(i).getContact() != null) {
+                    String mailAux = listInvitation.get(i).getContact().getEmail();
+                    long idClimateAux = listInvitation.get(i).getStudy().getId();
+                    for(int h=0; h<listInvitation.size(); h++){
+                        if((listInvitation.get(h).getState() != null) && (listInvitation.get(h).getContact() != null)){
+                            if(listInvitation.get(h).getContact().getEmail().equals(mailAux)
+                            && listInvitation.get(h).getStudy().getId().equals(idClimateAux)
+                            && listInvitation.get(h).getState().equals("Listo")){
+                                listInvReady.add(listInvitation.get(h));
+                                break;
+                            }
+                        }
                     }
-//                    if(!isReady){
-//                        listReal.add(cs);
-//                    }
+                }
+            }
+            
+            //Mediante las encuestas que están listas, obtenemos las encuestas que han sido enviadas
+            //pero que aún no están listas
+            //Genera datos repetidos!
+            List<ClimateStudyInvitation> listInvNotReady = new ArrayList<>();
+            System.out.println("Encuestas que ya están listas: "+listInvReady.size());
+            listInvNotReady = listInvitation;
+            for(int i = 0; i<listInvReady.size(); i++){
+                if(listInvReady.get(i).getContact() != null)
+                    for(int j=0; j<listInvitation.size(); j++)
+                        if(listInvitation.get(j).getContact() != null)
+                            if(listInvitation.get(j).getContact().equals(listInvReady.get(i).getContact()) 
+                            && listInvitation.get(j).getStudy().getId().equals(listInvReady.get(i).getStudy().getId()))
+                                listInvNotReady.remove(listInvitation.get(j));
+                            
+            }
+            System.out.println("Encuestas no listas(cantidad de encuestas enviadas sin ser respondidas): "+listInvNotReady.size());
+            
+            //Esta es la función que envía los correos de recordatorio
+            //sendReminder y urisTemp se utiliza para no enviar más de una vez el mensaje
+            //debido a duplicidad de los mismos datos
+            List<String> urisTemp = new ArrayList<>();
+            for(int i = 0; i<listInvNotReady.size(); i++){
+                boolean sendReminder = true;
+                if(listInvNotReady.get(i).getContact() != null){
+                    String uri = cypher.crypt(listInvNotReady.get(i).getContact().getEmail() + "||" + listInvNotReady.get(i).getStudy().getId().toString());
                     
-//                    if (!cs.getState().equals("Listo")) {
-//                        String uri = cypher.crypt(cs.getContact().getEmail() + "||" + cs.getStudy().getId().toString());
-//                        
-//                        String mensaje = "Estimado " + cs.getContact().getName() + " aun no ha contestado la encuesta del estudio " + cs.getStudy().getName() + ".\n"
-//                                + "El Sistema Revalora te solicita que respondas las preguntas en el siguiente enlace:\n\n" + basePath + "?survey=" + uri;
-//                        //emailSessionBean.sendMail(cs.getContact().getEmail(), "", "", "", "Recordatorio de Revalora", mensaje);
-//                        System.out.println(mensaje);
-//                    }
-                }                
+                    if(urisTemp.contains(uri) && !urisTemp.isEmpty())
+                        sendReminder = false;
+                    else
+                        urisTemp.add(uri);
+                    if(sendReminder){
+                        String mensaje = "Estimado " + listInvNotReady.get(i).getContact().getName() + " aun no ha contestado la encuesta del estudio " + listInvNotReady.get(i).getStudy().getName() + ".\n"
+                             + "El Sistema Revalora te solicita que respondas las preguntas en el siguiente enlace:\n\n" + basePath + "?survey=" + uri;
+                        emailSessionBean.sendMail(listInvNotReady.get(i).getContact().getEmail(), "", "", "", "Recordatorio de Revalora", mensaje);
+                        System.out.println(mensaje);
+                    }     
+                }
             }
-            System.out.println("hola");
-            for(ClimateStudyInvitation lr: listReal){
-                System.out.println(lr.getStudy().getId() + " " + lr.getContact().getEmail() + lr.getState());
-            }
+           if(listInvNotReady.size()>0)
+               JsfUtil.addSuccessMessage("Recordatorio enviado");
+           if(listInvNotReady.isEmpty())
+               JsfUtil.addSuccessMessage("Toas las encuestas enviadas están respondida, no es necesario enviar recordatorio");
         } catch (Exception ex) {
+            Logger.getLogger(resendInvitation.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("* Error: LittleMailer::sendInvitationToSurvey: No ha sido posible enviar el correo.");
         }
     }
 
 }
+
